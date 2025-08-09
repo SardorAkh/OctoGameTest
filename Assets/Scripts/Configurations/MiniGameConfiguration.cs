eec8f5880ed0f84863f5959d0fadc2915b5bd3db
Sardor Akhmadjonov <sardorakh7@gmail.com>
2025-08-09 11:36:56 +0500

    Update: Custom Configurations added

---
+++ Assets/Scripts/Configurations/MiniGameConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Naninovel;
+using UnityEngine;
+
+namespace Configurations
+{
+    [EditInProjectSettings]
+    public class MiniGameConfiguration : Configuration
+    {
+        [Header("Mini Games Settings")] public List<MiniGameData> miniGames = new();
+        [Header("UI Settings")] public Canvas miniGameCanvas;
+    }
+}
---
+++ Assets/Scripts/Configurations/MiniGameData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Configurations
+{
+    [Serializable]
+    public class MiniGameData
+    {
+        public string gameId;
+
+        public string displayName;
+
+        public GameObject uiPrefab;
+
+        public int winThreshold = 50;
+
+        public float maxDuration = 60f;
+
+        public bool isReplayable = true;
+    }
+}
